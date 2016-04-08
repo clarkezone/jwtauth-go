e34e37a2dcab0bd5bdbebed49c1bf40b4185ba98
James Clarke <james@clarkezone.net>
2016-04-08 05:49:57 -0700

    Support CORS

--- authmiddleware.go
+++ authmiddleware.go
@@ -21,3 +21,20 @@ func RequireTokenAuthentication(fn http.HandlerFunc) http.HandlerFunc {
 		}
 	}
 }
+
+func CorsOptions(fn http.HandlerFunc) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			rw.Header().Set("Access-Control-Allow-Headers", "accepts, authorization, content-type, x-api-applicationid, access-control-allow-origin")
+			rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+			rw.Header().Set("Access-Control-Allow-Origin", "*")
+			rw.WriteHeader(http.StatusOK)
+			// Do not run the passed in function in this branch as we don't want to continuing the hander pipeline
+			return
+		} else {
+			rw.Header().Set("Access-Control-Allow-Origin", "*")
+			fn(rw, r)
+		}
+	}
+
+}
